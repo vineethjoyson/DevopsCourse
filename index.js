a4c1d5a0053289a6a4633e1f9e412c39ad0e2162
vineeth <vineethjoyson4@gmail.com>
2025-09-18 11:37:36 +0530

    setup CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("hello Devops");
+console.log("Im learning CI CD pipeline");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("starting the tests....");
+setTimeout(() => console.log("Waiting 3 seconds...."), 3000);
+console.log("test completed");
